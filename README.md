79e8d319990b95c5b312f82ad30ab09b1f6bf0c7
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-08-22 08:13:26 +0200

    Add Fabrik CI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fabrik
+
+![Build Status](https://github.com/skovmand/fabrik/workflows/Fabrik%20CI/badge.svg)
+
 A Sudoku solver with backtracking written in Rust
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn can_calculate() {
+        let value = 4;
+        assert_eq!(value, 4);
+    }
+}
