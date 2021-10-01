ff6b6920d53cd080eb232389bb400a2e5e932746
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-10-01 23:50:55 +0200

    Remember the current position (#11)
    * Remember the current position
    
    * Rename first_free_field -> next_free_field
    
    * Add a few comments
    
    * Remove unneeded type conversions from Position
    
    * Bugfix: Final number not rendered
    
    * Initialization function for board solving
    
    * Rename next_free_field -> next_empty_field

--- src/lib.rs
+++ src/lib.rs
@@ -2,43 +2,51 @@ pub mod renderers;
 pub mod sudoku;
 
 use renderers::SudokuRenderer;
-use sudoku::{SudokuBoard, SudokuError, SudokuField};
+use sudoku::{Position, SudokuBoard, SudokuError, SudokuField};
 
+/// Solve a SudokuBoard with callback to a SudokuRenderer at every step
+pub fn solve_board<T: SudokuRenderer>(
+    board: &mut SudokuBoard,
+    renderer: &T,
+) -> Result<SudokuBoard, SudokuError> {
+    do_solve_board(board, renderer, Position { row: 0, column: 0 })
+}
+
+// Solve the Sudoku Board
 // Backtracking here is at once very advanced and also really simple!
 // For every iteration we are finding the first empty field on the board,
 // scanning row, column and square to check if a number is valid,
 // then placing the number in the field. After this we will recurse by calling
 // the same function once again, but below the recursion we will put back None
 // into the field, so if the program backtracks, it will restore the original state.
-pub fn solve_board<T: SudokuRenderer>(
+fn do_solve_board<T: SudokuRenderer>(
     board: &mut SudokuBoard,
     renderer: &T,
+    position: Position,
 ) -> Result<SudokuBoard, SudokuError> {
-    // Render the current board
-    renderer.display_step(board);
+    for num in 1..=9 {
+        let field = SudokuField::Value(num);
 
-    match board.first_free_field() {
-        // There are no more free fields. The board is solved!
-        None => Ok(board.clone()),
+        if board.valid_number(&position, &field) {
+            board.put_field(&position, field);
+            renderer.display_step(board);
 
-        // We are still solving!
-        Some(position) => {
-            for num in 1..=9 {
-                let field = SudokuField::Value(num);
-                if board.valid_number(&position, &field) {
-                    board.put_field(&position, field);
-
-                    if let Ok(board) = solve_board(board, renderer) {
-                        return Ok(board);
-                    }
-
-                    board.put_field(&position, SudokuField::Empty);
+            if let Some(position) = board.next_empty_field(&position) {
+                // Another free field is present, let's jump to that and continue solving
+                if let Ok(board) = do_solve_board(board, renderer, position) {
+                    return Ok(board);
                 }
-            }
 
-            Err(SudokuError::Unsolvable)
+                // We met a dead end, set the field to Empty again (after which we will continue looping)
+                board.put_field(&position, SudokuField::Empty);
+            } else {
+                // The sudoku has no more empty fields, we are done!
+                return Ok(board.clone());
+            };
         }
     }
+
+    Err(SudokuError::Unsolvable)
 }
 
 #[cfg(test)]
